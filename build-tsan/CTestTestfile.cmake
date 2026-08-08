472133fc541e73242a0713581f3f222c029cccdc
wzyou <wzyou.goo@gmail.com>
2026-08-08 16:58:11 +0800

    add .gitignore

--- build-tsan/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/weyou/work/netp2
-# Build directory: /Users/weyou/work/netp2/build-tsan
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test("netp2_smoke_test" "/Users/weyou/work/netp2/build-tsan/netp2_smoke_test")
-set_tests_properties("netp2_smoke_test" PROPERTIES  _BACKTRACE_TRIPLES "/Users/weyou/work/netp2/CMakeLists.txt;42;add_test;/Users/weyou/work/netp2/CMakeLists.txt;0;")
--- build/CMakeFiles/netp2_gateway.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for netp2_gateway.
-# This may be replaced when dependencies are built.
